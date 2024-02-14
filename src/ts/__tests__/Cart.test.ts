37367495493c223102016363102cc0d60c2e9318
ZavyalovAndrei <zaver1983@gmail.com>
2024-02-14 14:37:04 +0100

    simplify test in Cart.ts

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -4,7 +4,14 @@ import MusicAlbum from "../domain/MusicAlbum";
 import Cinema from "../domain/Cinema";
 
 let cart = new Cart();
-cart.add(new Book(1001, "Librarian", "Mikhail Elizarov", 2007, 600));
+
+test("new card should be empty", () => {
+  const testCart = new Cart();
+  expect(testCart.items.length).toBe(0);
+});
+
+test("should be 3 positions in the card when they are added", () => {
+  cart.add(new Book(1001, "Librarian", "Mikhail Elizarov", 2007, 600));
 cart.add(new MusicAlbum(1002, "Grave Digger", "Mob Tactics", 1200));
 cart.add(
   new Cinema(
@@ -21,20 +28,11 @@ cart.add(
     400
   )
 );
-
-test("new card should be empty", () => {
-  const testCart = new Cart();
-  expect(testCart.items.length).toBe(0);
-});
-
-test("should be 3 positions in the card when they are added", () => {
   expect(cart.items.length).toBe(3);
 });
 
 test("should be return total summ (2200) of items", () => {
-  const result: number = cart.summ;
-  const expected: number = 2200;
-  expect(result).toBe(expected)
+  expect(cart.summ).toBe(2200)
 });
 
 test("should be return total summ (2090) of items with discount 5%", () => {
